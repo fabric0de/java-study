a6ed7a905fc8c539df62b1d140a49f8179217cc5
fabric0de <solee3013@gmail.com>
2024-04-02 23:36:51 +0900

    Java 변수 동시 선언과 초기화

---
+++ src/variable/Var4.java
@@ -0,0 +1,14 @@
+package variable;
+
+public class Var4 {
+    public static void main(String[] args) {
+        int a, b; //변수 동시에 초기화
+
+        int e = 5; // 변수 선언과 초기화
+
+        int c = 1,d = 2; //한번에 선언과 초기화
+
+        //지역 변수는 변수 초기화 강제
+
+    }
+}
