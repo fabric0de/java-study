015b813cd9417d21c6358fbcf114b9e54eae820b
fabric0de <solee3013@gmail.com>
2024-04-04 23:58:15 +0900

    Java 변수 활용해서 출력하기

---
+++ src/variable/quiz/VarQuestion1.java
@@ -0,0 +1,14 @@
+package variable.quiz;
+
+public class VarQuestion1 {
+    public static void main(String[] args) {
+        //변수 활용하여 출력
+        int num1 = 4;
+        int num2 = 3;
+
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 / num2);
+    }
+}
