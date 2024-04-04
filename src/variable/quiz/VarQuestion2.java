3c8310d9b856af710f5570a534c3802414494f64
fabric0de <solee3013@gmail.com>
2024-04-04 23:58:56 +0900

    Java 변수 활용해서 출력하기2

---
+++ src/variable/quiz/VarQuestion2.java
@@ -0,0 +1,14 @@
+package variable.quiz;
+
+public class VarQuestion2 {
+    public static void main(String[] args) {
+        int sum;
+        int num1 = 10;
+        int num2 = 20;
+
+        sum = num1 + num2;
+
+        System.out.println(sum);
+
+    }
+}
