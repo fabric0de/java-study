a9d2a1c45fc0260eca25019ee597fec09ab4f632
fabric0de <solee3013@gmail.com>
2024-04-14 22:14:58 +0900

    Java 연산자 예시

---
+++ src/operator/ex/OperationEx2.java
@@ -0,0 +1,15 @@
+package operator.ex;
+
+public class OperationEx2 {
+    public static void main(String[] args) {
+        double va1 = 1.5;
+        double va2 = 2.5;
+        double va3 = 3.5;
+
+        double sum = va1 + va2 + va3;
+        double avg = sum / 3;
+
+        System.out.println(sum);
+        System.out.println(avg);
+    }
+}
